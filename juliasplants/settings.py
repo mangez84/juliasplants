c85c1fb30445e73c932603d0fcd46236cd970e56
Magnus Rosén <mangez84@gmail.com>
2021-10-25 17:05:30 +0000

    Configure stripe settings

--- juliasplants/settings.py
+++ juliasplants/settings.py
@@ -161,6 +161,15 @@ else:
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
+
+# Stripe configuration
+
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
