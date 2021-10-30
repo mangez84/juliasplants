6b6ea89abfbbb5116eca2dae0681af62f6fed247
Magnus Rosén <mangez84@gmail.com>
2021-10-30 15:46:17 +0000

    Modify template and view to render plants in Bootstrap cards

--- plants/views.py
+++ plants/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Plant, Category
 
 
 def get_plants(request):
     """Return all plants."""
+    plants = Plant.objects.all()
+    print(plants)
+    context = {
+        'plants': plants,
+    }
     template = 'plants/plants.html'
-    return render(request, template)
+    return render(request, template, context)
