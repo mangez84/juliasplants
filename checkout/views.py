a39aec96629b4d1d501371ded19ed822be94979c
Magnus Rosén <mangez84@gmail.com>
2021-11-05 16:17:29 +0000

    Modify the checkout view to save orders in the database

--- checkout/views.py
+++ checkout/views.py
@@ -8,18 +8,56 @@ import stripe
 
 
 def checkout(request):
-    """Checkout and place order."""
+    """Checkout and save the order."""
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        return redirect('cart')
+        cart_items, total_cost = get_cart_items(request)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.total_cost = total_cost
+            order.stripe_pid = 'Test'
+            order.save()
+            for cart_item in cart_items:
+                order_item = OrderItem(
+                    order=order,
+                    plant=cart_item['plant'],
+                    quantity=cart_item['quantity'],
+                    total=cart_item['total'],
+                )
+                order_item.save()
+        return redirect('show_cart')
+
     cart_items, total_cost = get_cart_items(request)
-    form = OrderForm()
+    stripe_total = round(total_cost * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if request.user.is_authenticated:
+        form = OrderForm(initial={
+            'first_name': 'Test',
+            'last_name': 'Test',
+            'email': 'Test',
+            'address': 'Test',
+            'city': 'Test',
+            'postcode': 'Test',
+            'country': 'SE',
+            'phone_number': 'Test',
+        })
+    else:
+        form = OrderForm()
+
     context = {
         'cart_items': cart_items,
         'total_cost': total_cost,
         'form': form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     template = 'checkout/checkout.html'
     return render(request, template, context)
