16dfc8ccb203217fe57c95343ba3472796568127
Magnus Rosén <mangez84@gmail.com>
2021-11-14 17:52:47 +0000

    Create a model to store blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    """Display blog posts on the admin site."""
+    list_display = ('title', 'body', 'date', 'profile', 'plant',)
+    ordering = ('-date',)
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from profiles.models import UserProfile
+from plants.models import Plant
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    """A model to store blog posts."""
+    class Meta:
+        verbose_name_plural = 'Blog Posts'
+
+    title = models.CharField(max_length=150, null=False, blank=False)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, null=False, blank=False)
+    plant = models.ForeignKey(
+        Plant, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.title)
