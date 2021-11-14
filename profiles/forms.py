15cdba117700480263a13f8d86c9a7e20b4aca5c
Magnus Rosén <mangez84@gmail.com>
2021-11-14 14:19:19 +0000

    Reference the user model according to Django best practices

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth.models import User
 from .models import UserProfile, UserProfileComment
+
+User = get_user_model()
 
 
 class UserForm(forms.ModelForm):
