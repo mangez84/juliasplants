54256600006d1a24f62f54c3bebbed761967cbf4
Magnus Rosén <mangez84@gmail.com>
2021-11-14 11:54:27 +0000

    Display user comments on the admin site

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserProfileComment
 
 
-class ProfileAdmin(admin.ModelAdmin):
+class UserProfileAdmin(admin.ModelAdmin):
+    """Display user profiles on the admin site."""
     list_display = ('user', 'address', 'city', 'postcode', 'country',)
     ordering = ('user',)
 
 
-admin.site.register(UserProfile, ProfileAdmin)
+class UserProfileCommentAdmin(admin.ModelAdmin):
+    """Display user profile comments on the admin site."""
+    list_display = ('title', 'comment', 'rating', 'profile',)
+    ordering = ('profile',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserProfileComment, UserProfileCommentAdmin)
