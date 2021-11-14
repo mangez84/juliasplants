562bc57e2d2956f58f238ed0fbaa50c7704facc3
Magnus Rosén <mangez84@gmail.com>
2021-11-14 11:27:56 +0000

    Create a model for user comments and ratings

--- profiles/models.py
+++ profiles/models.py
@@ -18,3 +18,16 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class UserProfileComment(models.Model):
+    """A model to store user comments and ratings."""
+    class Meta:
+        verbose_name_plural = 'User Profile Comments'
+
+    title = models.CharField(max_length=50, null=False, blank=False)
+    comment = models.CharField(max_length=150, null=False, blank=False)
+    rating = models.IntegerField(null=False, blank=False, default=1)
+
+    def __str__(self):
+        return str(self.title)
