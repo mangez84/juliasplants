a728929968b8da350375a7492d35a9c29ab389c5
Magnus Rosén <mangez84@gmail.com>
2021-11-16 18:57:24 +0000

    Modify view to render blog posts in the template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 
 def show_blog_posts(request):
     """Return blog posts."""
+    blog_posts = BlogPost.objects.all()
+    context = {
+        'blog_posts': blog_posts,
+    }
     template = 'blog/blog_posts.html'
-    return render(request, template)
+    return render(request, template, context)
